2555430a980ab9ee222eabcd6b8374f4d5fd234b
Sergey Karinsky <karinsky.dev@gmail.com>
2023-10-24 19:17:50 +0700

    Update tickets page data filter

--- src/pages/Tickets/index.jsx
+++ src/pages/Tickets/index.jsx
@@ -8,48 +8,62 @@ import { fetchTicketGroups, fetchAllTickets, getMatchTickets } from '../../redux
 import { fetchData, getSchedule } from '../../redux/data'
 import { getIsAdmin } from '../../redux/user'
 import { getColumnSearchProps } from '../../utils/components'
-
-const matchColumns = [
-  {
-    title: 'Tournament',
-    dataIndex: 'match',
-    key: 'tournament',
-    render: match => match.tournament && match.tournament?.en,
-    sorter: (a, b) => a.match?.tournament?.en?.localeCompare(b.match?.tournament?.en),
-    ...getColumnSearchProps('tournament', record => record.match?.tournament?.en)
-  },
-  {
-    title: 'Match',
-    dataIndex: 'match',
-    key: 'match',
-    render: match => `${match.team1.en} — ${match.team2.en}`,
-    sorter: (a, b) => `${a.match?.team1?.en} — ${a.match?.team2?.en}`.localeCompare(`${b.match?.team1?.en} — ${b.match?.team2?.en}`),
-    ...getColumnSearchProps('match', record => `${record.match?.team1?.en} — ${record.match?.team2?.en}`)
-  },
-  {
-    title: 'Tickets',
-    dataIndex: 'tickets',
-    key: 'tickets',
-    render: tickets => `${tickets.length} ${tickets.length === 1 ? 'ticket' : 'tickets'}`
-  }
-]
+import { getOptions } from '../../utils/utils'
 
 function MatchList({ schedule }) {
   const navigate = useNavigate()
   const groups = useSelector(getMatchTickets)
   const isLoading = useSelector(state => state.tickets.isLoading)
   
-  const items = useMemo(() =>
-    groups.map(group => ({
+  const { items, homeOptions, awayOptions, tournamentOptions } = useMemo(() => {
+    const items = groups.map(group => ({
       ...group,
       match: schedule[group.matchId]
     }))
-    .filter(item => item.match)
-  , [groups, schedule])
+      .filter(item => item.match)
+    const homeOptions = getOptions(items, 'match.team1.en')
+    const awayOptions = getOptions(items, 'match.team2.en')
+    const tournamentOptions = getOptions(items, 'match.tournament.en')
+
+    return { items, homeOptions, awayOptions, tournamentOptions }
+  }, [groups, schedule])
+
+  const columns = [
+    {
+      title: 'Tournament',
+      dataIndex: 'match',
+      key: 'tournament',
+      render: match => match.tournament && match.tournament?.en,
+      sorter: (a, b) => a.match?.tournament?.en?.localeCompare(b.match?.tournament?.en),
+      ...getColumnSearchProps(record => record.match?.tournament?.en, { options: tournamentOptions })
+    },
+    {
+      title: 'Team home',
+      dataIndex: 'match',
+      key: 'home',
+      render: match => match.team1.en,
+      sorter: (a, b) => (a.match?.team1?.en || '').localeCompare(b.match?.team1?.en),
+      ...getColumnSearchProps(record => record.match?.team1?.en, { options: homeOptions })
+    },
+    {
+      title: 'Team away',
+      dataIndex: 'match',
+      key: 'away',
+      render: match => match.team2.en,
+      sorter: (a, b) => (a.match?.team2?.en || '').localeCompare(b.match?.team2?.en),
+      ...getColumnSearchProps(record => record.match?.team2?.en, { options: awayOptions })
+    },
+    {
+      title: 'Tickets',
+      dataIndex: 'tickets',
+      key: 'tickets',
+      render: tickets => `${tickets.length} ${tickets.length === 1 ? 'ticket' : 'tickets'}`
+    }
+  ]
 
   return (
     <Table
-      columns={matchColumns}
+      columns={columns}
       dataSource={items}
       loading={isLoading}
       rowKey={({ matchId }) => matchId}
