896c84eee3a2faa0ef2a97af8447ffb3d8f95079
Sergey Karinsky <karinsky.dev@gmail.com>
2023-12-22 17:41:06 +0700

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import { useEffect, useState } from 'react'
-import { LoadingOutlined } from '@ant-design/icons'
 import { Row } from 'antd'
+import { LoadingOutlined } from '@ant-design/icons'
 import { Route, Routes, useNavigate, useLocation } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-import PageContent from './pages/Content'
 import PageLogin from './pages/Login'
-import PageMatch from './pages/Match'
-import PageMatches from './pages/Matches'
-import PageNotifications from './pages/Notifications'
-import PageStadium from './pages/Stadium'
-import PageStadiums from './pages/Stadiums'
-import PageTeam from './pages/Team'
-import PageTeams from './pages/Teams'
-import PageTemplates from './pages/Templates'
-import PageTournament from './pages/Tournament'
-import PageTournaments from './pages/Tournaments'
-import PageTickets from './pages/Tickets'
-import PageTranslations from './pages/Translations'
 import PageUsers from './pages/Users'
 import PageUser from './pages/User'
 import Sendings from './pages/Sendings'
@@ -65,22 +52,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/translations" element={<PageTranslations />} />
-          <Route path="/content/:page" element={<PageContent />} />
-          <Route path="/templates/:page" element={<PageTemplates />} />
           <Route path="/users" element={<PageUsers />} />
           <Route path="/users/:id" element={<PageUser />} />
-          <Route path="/matches" element={<PageMatches />} />
-          <Route path="/matches/:id" element={<PageMatch />} />
-          <Route path="/notifications" element={<PageNotifications />} />
-          <Route path="/teams" element={<PageTeams />} />
-          <Route path="/teams/:id" element={<PageTeam />} />
-          <Route path="/stadiums" element={<PageStadiums />} />
-          <Route path="/stadiums/:id" element={<PageStadium />} />
-          <Route path="/tournaments" element={<PageTournaments />} />
-          <Route path="/tournaments/:id" element={<PageTournament />} />
-          <Route path="/tickets/:matchId?" element={<PageTickets />} />
-
           <Route path="/sendings" element={<Sendings isSendingAir={isSendingAir} setIsSendingAir={setIsSendingAir} />} />
           <Route path="/sendings/:sendingId" element={<Sending isSendingAir={isSendingAir} />} />
           <Route path="/sendings/:sendingId/:placeId" element={<Place />} />
